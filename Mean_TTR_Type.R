625d22b0698827ceb0e5b1e31b3736f4fc700248
smp201804 <38119898+smp201804@users.noreply.github.com>
2018-07-30 20:30:28 -0400

    Updating Mean_TTR_Type R Code

--- Mean_TTR_Type.R
+++ Mean_TTR_Type.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+
+# Fossil Fuel Steam
 ff_steam_df <- read_csv("ff_steam_expt.csv")
 
 # Filter for only forced, not planned or reserve shutdowns, outages and derates
@@ -8,9 +10,44 @@ ff_steam_df <- ff_steam_df %>%
 
 # Calculate mean Time_To_Repair for each outage Type
 ff_steam_df %>% group_by(Type) %>%
-  summarise(Mean_TTR_Type = mean(Time_To_Repair, na.rm = TRUE)) %>%
-  arrange(desc(Mean_TTR_Type))
+  summarise(Mean_TTR_Type_ff = mean(Time_To_Repair, na.rm = TRUE)) %>%
+  arrange(desc(Mean_TTR_Type_ff))
 
 # Create a Box Plot graph, x = Type, y = Time_To_Repair
-ggplot(data = ff_steam_df, mapping = aes(x = Type, y = Time_To_Repair)) +
-  geom_boxplot()
+ggplot(data = ff_steam_df) +
+  geom_boxplot(mapping = aes(x = Type, y = Time_To_Repair))
+
+#Combined Cycle
+cc_df <- read_csv("c_cycle_expt.csv")
+# Filter for only forced, not planned or reserve shutdowns, outages and derates
+cc_df <- cc_df %>%
+  filter(cc_df$Type %in% type_filter)
+
+# Calculate mean Time_To_Repair for each outage Type
+cc_df %>% group_by(Type) %>%
+  summarise(Mean_TTR_Type_cc = mean(Time_To_Repair, na.rm = TRUE)) %>%
+  arrange(desc(Mean_TTR_Type_cc))
+
+# Create a Box Plot graph, x = Type, y = Time_To_Repair
+ggplot(data = cc_df) +
+  geom_boxplot(mapping = aes(x = Type, y = Time_To_Repair))
+
+# Simple Cycle
+ss_df <- read_csv("s_cycle_expt.csv")
+
+# Filter for only forced, not planned or reserve shutdowns, outages and derates
+ss_df <- ss_df %>%
+  filter(ss_df$Type %in% type_filter)
+
+# Calculate mean Time_To_Repair for each outage Type
+ss_df %>% group_by(Type) %>%
+  summarise(Mean_TTR_Type_ss = mean(Time_To_Repair, na.rm = TRUE)) %>%
+  arrange(desc(Mean_TTR_Type_ss))
+
+# Create a Box Plot graph, x = Type, y = Time_To_Repair
+ggplot(data = ss_df) +
+  geom_boxplot(mapping = aes(x = Type, y = Time_To_Repair))
+
+  
+
+
